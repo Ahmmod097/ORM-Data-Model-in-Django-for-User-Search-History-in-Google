b0c7c1e37b5c9b63dc7396a5c18749bea00f3a02
Ahmmod097 <rifatsadique@gmail.com>
2021-09-19 23:51:53 +0630

    7th Push

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 from django.contrib.auth import views as auth_views
 urlpatterns = [
-    path('/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('signup/', views.userRegister, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
